e8a65e162f73c7c641f5df45199b927cb0e593b1
jiro4989 <jiroron666@gmail.com>
2019-05-20 22:37:10 +0900

    :construction: 一旦コミット

--- examples/write_file/main.nim
+++ examples/write_file/main.nim
@@ -4,10 +4,8 @@ var smfObj = newSMF(format0, 480)
 
 block:
   var track = newTrackChunk()
-  track.add newMIDIEvent(0, statusNoteOn, 0, 0, 10)
-  track.add newMIDIEvent(280, statusNoteOff, 0, 0, 0)
-  track.add newMIDIEvent(280, statusNoteOn, 0, 0, 10)
-  track.add newMIDIEvent(280, statusNoteOff, 0, 0, 0)
+  track.add newMIDIEvent(0, statusNoteOn, 0, 1, 40)
+  track.add newMIDIEvent(240, statusNoteOff, 0, 1, 0)
   smfObj.add track
 
 writeSMFFile("test.mid", smfObj)
