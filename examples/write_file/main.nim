ff2b384145c6313465c8593116963f2da38ac840
jiro4989 <jiroron666@gmail.com>
2019-05-31 17:21:14 +0900

    :recycle: change sample code

--- examples/write_file/main.nim
+++ examples/write_file/main.nim
@@ -1,18 +1,16 @@
 import smf
+import strformat
 
-var smfObj = newSMF(format0, 480)
+for i in 1..128:
+  var smfObj = newSMF(format0, 480)
 
-block:
   var track = newTrackChunk()
-  track.add newMetaEvent(0, metaText, "Test text")
-  for i in 1'u8..20:
-    let n: byte = 0x30'u8 + i
+  #track.add newMetaEvent(0, metaText, "Test text")
+  for n in 30'u8..50:
     track.add newMIDIEvent(0, statusNoteOn, 0, n, 0x64)
     track.add newMIDIEvent(120, statusNoteOff, 0, n, 0)
   smfObj.add track
 
-writeSMFFile("test.mid", smfObj)
-
-let smfObj2 = readSMFFile("test.mid")
-echo smfObj
-echo smfObj2
+  let outfn = &"out/{i:03}.mid"
+  writeSMFFile(outfn, smfObj)
+  echo outfn, " is generated."
