d77e3a31e0f7f4104b08ac3a86ac482ce990fe05
jiro4989 <jiroron666@gmail.com>
2020-03-25 08:54:53 +0900

    書き方改善

--- src/smf/smfwrite.nim
+++ src/smf/smfwrite.nim
@@ -5,19 +5,28 @@ type
     fileName: string
     data: Stream
 
-proc writeNoteOff*(self: SMFWrite, channelNumber, note: byte) =
-  ## 3 byte
+proc writeNoteOff*(self: SMFWrite, channel, note: byte) =
+  ## 3 byte (8n kk vv)
   # 8n
-  self.data.write(0b1000_0000'u8 and (channelNumber and 0b0000_1111'u8))
+  self.data.write(0x80'u8 and (channel and 0x0F'u8))
   # kk
   self.data.write(note)
   # vv
   self.data.write(0'u8)
 
-proc writeNoteOn*(self: SMFWrite, channelNumber, note, velocity: byte) =
-  ## 3 byte
+proc writeNoteOn*(self: SMFWrite, channel, note, velocity: byte) =
+  ## 3 byte (9n kk vv)
   # 9n
-  self.data.write(0b1001_0000'u8 and (channelNumber and 0b0000_1111'u8))
+  self.data.write(0x90'u8 and (channel and 0x0F'u8))
+  # kk
+  self.data.write(note)
+  # vv
+  self.data.write(velocity)
+
+proc writePolyphonicKeyPressure*(self: SMFWrite, channel, note, velocity: byte) =
+  ## 3 byte (An kk vv)
+  # An
+  self.data.write(0xA0'u8 and (channel and 0x0F'u8))
   # kk
   self.data.write(note)
   # vv
