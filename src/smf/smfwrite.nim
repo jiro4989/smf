67e7dab444ed1666f86b3d03f6cb8ac3dfcbc302
jiro4989 <jiroron666@gmail.com>
2020-03-31 00:24:37 +0900

    track.dataに変更

--- src/smf/smfwrite.nim
+++ src/smf/smfwrite.nim
@@ -7,7 +7,6 @@ type
     fileName: string
     header: HeaderChunk
     track: TrackChunk
-    data: Stream
 
 proc midiStatusByte(status: Status, channel: byte): byte =
   result = status and (channel and 0x0F'u8)
@@ -15,89 +14,89 @@ proc midiStatusByte(status: Status, channel: byte): byte =
 proc writeMidiNoteOff*(self: SmfWrite, timeNum: uint32, channel, note: byte) =
   ## 3 byte (8n kk vv)
   # delta time
-  self.data.write(timeNum.toDeltaTime)
+  self.track.data.write(timeNum.toDeltaTime)
   # MIDI event
   # 8n
-  self.data.write(midiStatusByte(stNoteOff, channel))
+  self.track.data.write(midiStatusByte(stNoteOff, channel))
   # kk
-  self.data.write(note)
+  self.track.data.write(note)
   # vv
-  self.data.write(0'u8)
+  self.track.data.write(0'u8)
 
 proc writeMidiNoteOn*(self: SmfWrite, timeNum: uint32, channel, note, velocity: byte) =
   ## 3 byte (9n kk vv)
   # delta time
-  self.data.write(timeNum.toDeltaTime)
+  self.track.data.write(timeNum.toDeltaTime)
   # MIDI event
   # 9n
-  self.data.write(midiStatusByte(stNoteOn, channel))
+  self.track.data.write(midiStatusByte(stNoteOn, channel))
   # kk
-  self.data.write(note)
+  self.track.data.write(note)
   # vv
-  self.data.write(velocity)
+  self.track.data.write(velocity)
 
 proc writeMidiPolyphonicKeyPressure*(self: SmfWrite, timeNum: uint32, channel, note, velocity: byte) =
   ## 3 byte (An kk vv)
   # delta time
-  self.data.write(timeNum.toDeltaTime)
+  self.track.data.write(timeNum.toDeltaTime)
   # MIDI event
   # An
-  self.data.write(midiStatusByte(stPolyphonicKeyPressure, channel))
+  self.track.data.write(midiStatusByte(stPolyphonicKeyPressure, channel))
   # kk
-  self.data.write(note)
+  self.track.data.write(note)
   # vv
-  self.data.write(velocity)
+  self.track.data.write(velocity)
 
 proc writeMidiControlChange*(self: SmfWrite, timeNum: uint32, channel, controller, value: byte) =
   ## 3 byte (Bn cc vv) 特殊なので注意
   # delta time
-  self.data.write(timeNum.toDeltaTime)
+  self.track.data.write(timeNum.toDeltaTime)
   # MIDI event
   # Bn
-  self.data.write(midiStatusByte(stControlChange, channel))
+  self.track.data.write(midiStatusByte(stControlChange, channel))
   # cc
-  self.data.write(controller)
+  self.track.data.write(controller)
   # vv
-  self.data.write(value)
+  self.track.data.write(value)
 
 proc writeMidiProgramChange*(self: SmfWrite, timeNum: uint32, channel, program: byte) =
   ## 2 byte (Cn pp)
   # delta time
-  self.data.write(timeNum.toDeltaTime)
+  self.track.data.write(timeNum.toDeltaTime)
   # MIDI event
   # Cn
-  self.data.write(midiStatusByte(stProgramChange, channel))
+  self.track.data.write(midiStatusByte(stProgramChange, channel))
   # pp
-  self.data.write(program)
+  self.track.data.write(program)
 
 proc writeMidiChannelPressure*(self: SmfWrite, timeNum: uint32, channel, pressure: byte) =
   ## 2 byte (Dn pp)
   # delta time
-  self.data.write(timeNum.toDeltaTime)
+  self.track.data.write(timeNum.toDeltaTime)
   # MIDI event
   # Dn
-  self.data.write(midiStatusByte(stChannelPressure, channel))
+  self.track.data.write(midiStatusByte(stChannelPressure, channel))
   # pp
-  self.data.write(pressure)
+  self.track.data.write(pressure)
 
 proc writeMidiPitchBend*(self: SmfWrite, timeNum: uint32, channel, pitch1, pitch2: byte) =
   ## 2 byte (Dn pp) リトルエンディアンなので注意
   # delta time
-  self.data.write(timeNum.toDeltaTime)
+  self.track.data.write(timeNum.toDeltaTime)
   # MIDI event
   # En
-  self.data.write(midiStatusByte(stPitchBend, channel))
+  self.track.data.write(midiStatusByte(stPitchBend, channel))
   # ll
-  self.data.write(pitch1)
+  self.track.data.write(pitch1)
   # mm
-  self.data.write(pitch2)
+  self.track.data.write(pitch2)
 
 proc writeMetaEndOfTrack*(self: SmfWrite) =
   ## 4 byte
-  self.data.write(0'u8)             # delta time
-  self.data.write(stMetaPrefix)     # meta prefix
-  self.data.write(meEndOfTrack.ord) # end of track
-  self.data.write(0'u8)             # data length
+  self.track.data.write(0'u8)             # delta time
+  self.track.data.write(stMetaPrefix)     # meta prefix
+  self.track.data.write(meEndOfTrack.ord) # end of track
+  self.track.data.write(0'u8)             # data length
 
 proc newHeaderChunk(): HeaderChunk =
   result = HeaderChunk(
@@ -107,10 +106,11 @@ proc newHeaderChunk(): HeaderChunk =
     timeUnit: 0'u16,
   )
 
-proc newTrackChunk(): TrackChunk =
+proc newTrackChunk(filename: string): TrackChunk =
   result = TrackChunk(
     chunkType: trackChunkType,
+    data: newFileStream(filename, fmWrite),
   )
 
 proc openSmfWrite*(filename: string): SmfWrite =
-  result = SmfWrite(header: newHeaderChunk(), track: newTrackChunk())
+  result = SmfWrite(header: newHeaderChunk(), track: newTrackChunk(filename))
